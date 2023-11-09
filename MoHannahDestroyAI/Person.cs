752beb1bea5d460a4ae27e8b3dd5658b7159529a
Hannah Hong <hahong@microsoft.com>
2023-11-08 16:06:59 -0800

    people

---
+++ MoHannahDestroyAI/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        public Person(string name) 
+        { 
+            this.Name = name;
+        }
+
+        public Person(string name, int age) 
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ MoHannahDestroyAI/Team.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Team
+    {
+        private List<Person> people;
+
+        public Team(List<Person> people)
+        {
+            this.people = people;
+        }
+
+        public IEnumerable<Person> GetTeammatesByAge(int minAge, int maxAge)
+        {
+            foreach (Person person in this.people)
+            {
+                if (person.Age >= minAge && person.Age <= maxAge)
+                {
+                    yield return person;
+                }
+            }
+        }
+    }
+}
